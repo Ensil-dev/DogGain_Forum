05f4bb7881dba49c8c05cd429550dde68c237911
Ensil <lasags@naver.com>
2024-08-14 12:50:46 +0900

    추가) 글쓰기 모달 레이아웃: input/submit

--- forum_client/src/components/PostWritingForm.jsx
+++ forum_client/src/components/PostWritingForm.jsx
@@ -29,7 +29,7 @@ const CloseBtn = styled.div`
 
 export default function PostWritingForm({ handleWritingModal }) {
     return (
-        <Form>
+        <Form id='writingForm'>
             <div style={{ display: 'grid', gridTemplateColumns: '3fr 1fr', alignItems: 'center', backgroundColor: 'lightblue', height: '30px', padding: '10px 0px' }}>
                 <div>게시판 글쓰기</div>
                 <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
@@ -40,7 +40,8 @@ export default function PostWritingForm({ handleWritingModal }) {
             </div>
             <div>
                 <input minLength='10' autofocus required style={{ width: '100%', height: '50px' }} placeholder='제목 (특수문자 금지입니다)'></input>
-                <input required style={{ display: 'flex', width: '100%', height: '90vh' }}></input>
+                <input required style={{ display: 'flex', width: '100%', height: '80vh' }}></input>
+                <input type='submit' value='Submit'></input>
             </div>
         </Form>
     );
