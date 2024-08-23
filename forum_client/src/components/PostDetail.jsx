61e3f1471a8c4359fe69c08b1645044a2794ba66
Ensil <lasags@naver.com>
2024-08-23 18:07:58 +0900

    추가) 수정, 삭제 버튼

--- forum_client/src/components/PostDetail.jsx
+++ forum_client/src/components/PostDetail.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import UnifiedDivider from './UnifiedDivider';
 import { useDispatch, useSelector } from 'react-redux';
 import { DEV_POST_URL } from '../api/api';
+import { FaEdit } from 'react-icons/fa';
+import { MdDeleteForever } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
 import PostDetailHeader from './PostDetailHeader';
 import styled from 'styled-components';
@@ -58,9 +60,20 @@ export default function PostDetail() {
 
             <UnifiedDivider $padding='0px 10px' $border='1px solid gray' $opacity='0.15' />
 
-            <div style={{ display: 'flex', width: '100%', padding: '12px 0px' }}>
-                <p style={{ padding: '0px 20px 0px 20px', lineHeight: 1.5, fontWeight: '500' }}>{postDetailInfo.profile.nickname}</p>
+            {/* <div> */}
+            <div style={{ display: 'grid', gridTemplateColumns: '9fr 1fr 1fr', alignContent: 'center', width: '100%', padding: '12px 0px', gap: '4px' }}>
+                <div style={{ width: '100%', padding: '0px 20px 0px 20px', lineHeight: 1.5, fontWeight: '500' }}>{postDetailInfo.profile.nickname}</div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', gap: '4px', border: '2px solid lightgray', cursor: 'pointer' }}>
+                    <FaEdit style={{ width: '24px', height: '24px', color: 'lightgray' }} />
+                    <div style={{ fontSize: 'small' }}>수정</div>
+                </div>
+
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', gap: '4px', marginRight: '20px', border: '2px solid lightgray', cursor: 'pointer' }}>
+                    <MdDeleteForever style={{ width: '24px', height: '24px', color: 'lightgray' }} />
+                    <div style={{ fontSize: 'small' }}>삭제</div>
+                </div>
             </div>
+            {/* </div> */}
 
             <UnifiedDivider $padding='0px 0px' $border='4px solid gray' $opacity='0.15' />
 
