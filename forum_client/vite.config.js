145a31e5a935858940f9d2a0e623f8aa3c1d4a0b
Ensil <lasags@naver.com>
2024-09-06 16:08:02 +0900

    추가) VITE build & deploy 설정

--- forum_client/vite.config.js
+++ forum_client/vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig ({
+	base:'DogGain_Forum',
 	plugins: [react()],
+	build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
